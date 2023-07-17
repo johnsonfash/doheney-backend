ed661f3fd97de2dbc76523ba861403841498c093
Johnson Fash <fashanutosin7@gmail.com>
2023-07-17 05:37:32 +0100

    added seeding script

--- prisma/migrations/20230717043233_dev/migration.sql
+++ prisma/migrations/20230717043233_dev/migration.sql
@@ -3,8 +3,7 @@ CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "hash" TEXT NOT NULL,
-    "first_name" TEXT,
-    "last_name" TEXT,
+    "name" TEXT,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
